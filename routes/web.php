6e55a388a282786e0041d20d4aa6c7b74ad30a5c
diarietou <diarietou713@gmail.com>
2025-07-20 21:51:42 +0000

    Résolution des conflits et correction des routes

--- routes/web.php
+++ routes/web.php
@@ -6,38 +6,24 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
-// Authentification
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Profil utilisateur
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [AuthController::class, 'showProfile'])->name('profile');
-    Route::post('/profile', [AuthController::class, 'updateProfile'])->name('profile.update');
-
-    Route::get('/password/change', [AuthController::class, 'showChangePasswordForm'])->name('password.change.form');
-    Route::post('/password/change', [AuthController::class, 'changePassword'])->name('password.change');
-});
-
-Route::get('/produits', [ProductController::class, 'index'])->name('products.index');
-Route::get('/produits/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::post('/panier/ajouter/{id}', [\App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
-=======
 // Authentification client
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/profile', [AuthController::class, 'showProfile'])->name('profile')->middleware('auth');
-Route::put('/profile', [AuthController::class, 'updateProfile'])->name('profile.update')->middleware('auth');
-Route::put('/profile/password', [AuthController::class, 'changePassword'])->name('profile.password')->middleware('auth');
 
+// Profil utilisateur
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [AuthController::class, 'showProfile'])->name('profile');
+    Route::post('/profile', [AuthController::class, 'updateProfile'])->name('profile.update');
+    Route::get('/password/change', [AuthController::class, 'showChangePasswordForm'])->name('password.change.form');
+    Route::post('/password/change', [AuthController::class, 'changePassword'])->name('password.change');
+});
+
+// Catalogue produit
 Route::get('/produits', [ProductController::class, 'index'])->name('products.index');
->>>>>>> 5e86a9d (catalogue produit)
+Route::get('/produits/{id}', [ProductController::class, 'show'])->name('products.show');
+
+// Panier
+Route::post('/panier/ajouter/{id}', [\App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
