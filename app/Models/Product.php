11134f670536b3c4bd3b8cc601240e0ff0677e86
diarietou <diarietou713@gmail.com>
2025-07-20 21:56:55 +0000

    Résolution du conflit dans Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-<<<<<<< HEAD
     protected $fillable = ['name', 'description', 'description_longue', 'price', 'image', 'stock', 'category_id'];
-=======
-    protected $fillable = ['name', 'description', 'price', 'image', 'stock', 'category_id'];
->>>>>>> 5e86a9d (catalogue produit)
 
     public function category()
     {
